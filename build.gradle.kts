c4369199751d163019723f5e64735c3c24ec46c0
AkosPaha <paha.akos@gmail.com>
2022-11-29 20:23:22 +0100

    More Changes

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+@file:Suppress("UNUSED_VARIABLE")
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://maven.fabric.io/public")
+        maven("https://maven.google.com")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.0-alpha08")
+        //noinspection DifferentKotlinGradleVersion
+        classpath(kotlin("gradle-plugin", version = "1.7.21"))
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://maven.fabric.io/public")
+        maven("https://maven.google.com")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "Deep Link Rboard"
+include(":app")
+include(":mathc")
+include(":colorutilsc")
+include(":rboardcomponents")
