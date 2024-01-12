afa7be8efeef0fce320cfb136c8cc5164b4e62c9
PahaAkos <paha.akos@gmail.com>
2024-01-12 22:42:49 +0100

    Monthly Update
    New dependencies changes:
    • Update com.android.tools.build:gradle to 8.4.0-alpha04
    • Update androidx.activity:activity-ktx to 1.9.0-alpha01
    • Update androidx.compose.ui:ui-tooling to 1.6.0-rc01
    • Update androidx.core:core-ktx to 1.13.0-alpha03
    • Update androidx.core:core to 1.13.0-alpha03
    • Update androidx.browser.browser to 1.8.0-beta01
    • Update androidx.fragment:fragment-ktx to 1.7.0-alpha08
    
    Others Changes:
    • Update versionCode to 114000
    • Update versionName to 1.1.4
    • Update kotlin to 1.9.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "de.dertyp7214.deeplinkrboard"
         minSdk = 23
         targetSdk = 34
-        versionCode = 113002
-        versionName = "1.1.3"
+        versionCode = 114000
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
